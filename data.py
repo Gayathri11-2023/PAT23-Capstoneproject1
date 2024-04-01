562c9c7b0c54b1c71b838bc72d9752c54d926c42
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-04-01 19:47:03 +0530

    Data uploaded

---
+++ data.py
@@ -0,0 +1,14 @@
+
+
+class WebData:
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username = "Admin"
+    password = "admin123"
+    wrongpassword = "Invalid password"
+    emp_firstname = "Teju"
+    emp_lastname = "sri"
+    emp_id ="101"
+    employeefirstname = "Raj"
+    employeelastname ="kumar"
+    empid ="emp101"
+    dashboardURL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
