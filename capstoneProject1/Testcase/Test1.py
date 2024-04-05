c029c38f60b03dbe2790ef900b2f801454c68e4a
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-04-05 13:59:57 +0530

    Update Test1.py

--- capstoneProject1/Testcase/Test1.py
+++ capstoneProject1/Testcase/Test1.py
@@ -9,13 +9,13 @@ from Locators import locator
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
 import pytest
-from time import sleep
 
 # Defining a test class
 
 
-class Test:
+class Testcase1:
     dashboard = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
 
     # pytest fixture for setting up the test environment
@@ -24,27 +24,28 @@ class Test:
         # Setting up Chrome WebDriver with the WebDriver Manager
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         yield
+        # implementation of implicit wait
+       self.driver.implicitly_wait(10)
         self.driver.quit()
 
     @pytest.mark.html
     def test_login(self, boot):
-        # Opening the specified URL in the browser
-        self.driver.get(data.WebData().url)
-        # Maximizing the browser window
-        self.driver.maximize_window()
-        # Introducing a sleep delay for 5 seconds
-        sleep(5)
+        try:
+           # Opening the specified URL in the browser
+           self.driver.get(data.WebData().url)
+           # Maximizing the browser window
+           self.driver.maximize_window()
+          
+           # Call the 'entertext' and 'clickbutton' method from the 'WebLocators' class to
+           # enter text into a username field , Password field and login button
 
-        # Call the 'entertext' and 'clickbutton' method from the 'WebLocators' class to
-        # enter text into a username field , Password field and login button
+           locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
+           locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator, data.WebData().password)
+           locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)
+           assert (self.driver.current_url == data.WebData().dashboardURL)
 
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator, data.WebData().password)
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)
-        assert (self.driver.current_url == data.WebData().dashboardURL)
-
-        # Print a success message indicating that login was successful, along with the username and password used
-        print(f"SUCCESS : Logged in with {data.WebData().username} and the password is {data.WebData().password}")
-        print("Test Case-1 : Valid username and password is evaluated")
-
-
+           # Print a success message indicating that login was successful, along with the username and password used
+           print(f"SUCCESS : Logged in with {data.WebData().username} and the password is {data.WebData().password}")
+           print("Test Case-1 : Valid username and password is evaluated")
+   except  NoSuchElementException as e:
+           print("error")
